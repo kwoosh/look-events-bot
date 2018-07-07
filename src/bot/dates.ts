d0950d0357d543583511fce825621e90bbb14656
Andrew Pashinnik <tobirawork@gmail.com>
2018-07-07 17:33:01 +0300

    feat(bot/dates): add format function

--- src/bot/dates.ts
+++ src/bot/dates.ts
@@ -3,3 +3,7 @@ import * as moment from 'moment'
 export function getRemindDate(exactDate: moment.MomentInput, days: number) {
     return moment(exactDate).subtract(days, 'days')
 }
+
+export function format(date: moment.Moment) {
+    return date.format('YYYY-MM-DD')
+}
