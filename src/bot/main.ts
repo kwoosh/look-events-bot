a4f2dba64446fac99bb141a2e64fb88b2373dfc7
Andrew Pashinnik <tobirawork@gmail.com>
2018-07-08 01:03:34 +0300

    feat(bot/main): add command handlers for help, about, reminders and hears for /rID to send reminder card

--- src/bot/main.ts
+++ src/bot/main.ts
@@ -3,20 +3,37 @@ import config from '../config'
 import db from '../db'
 import askForRemindDay from './actions/reminder/askForRemindDay'
 import newReminder from './actions/reminder/newReminder'
-import { sendCard } from './actions/send'
+import listReminders from './actions/reminder/listReminders'
+import { sendEventCard } from './actions/send/eventCard'
+import { sendReminderCard } from './actions/send/reminderCard'
 import { BUTTON_TYPES } from './buttons'
 import { commandReplies } from './messages'
 
 const bot = new Telegraf(config.BOT_TOKEN)
 
 bot.start(ctx => {
-    ctx.reply(commandReplies.start)
-
     db.addUser(ctx.from.id, ctx.from.username)
+    ctx.replyWithHTML(commandReplies.start)
+})
+
+bot.command('help', ctx => {
+    ctx.replyWithHTML(commandReplies.help)
+})
+
+bot.command('about', ctx => {
+    ctx.replyWithHTML(commandReplies.about)
+})
+
+bot.command('reminders', async ctx => {
+    listReminders(ctx)
+})
+
+bot.hears(/\/r(\d+)/, ctx => {
+    sendReminderCard(Number(ctx.match[1]), ctx)
 })
 
 bot.hears(/\/e(\d+)/, ctx => {
-    sendCard(Number(ctx.match[1]), ctx)
+    sendEventCard(Number(ctx.match[1]), ctx)
 })
 
 bot.on('callback_query', async ctx => {
@@ -30,6 +47,12 @@ bot.on('callback_query', async ctx => {
 
         case BUTTON_TYPES['reminder-new']:
             newReminder(Number(payload.eventID), Number(payload.days), Number(payload.msgID), ctx)
+            break
+
+        case BUTTON_TYPES['reminder-delete']:
+            // db.deleteReminder(Number(payload.reminderID)).then(() => {
+            //    console.log('deleted')
+            //})
             break
 
         default:
