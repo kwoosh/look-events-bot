1ce0bba644a4600099e228e45018e4d823e9a691
Andrew Pashinnik <tobirawork@gmail.com>
2018-07-07 17:36:47 +0300

    fix(send): imports and button_types

--- src/bot/actions/send.ts
+++ src/bot/actions/send.ts
@@ -1,12 +1,12 @@
 import { ContextMessageUpdate } from 'telegraf'
-import { api } from '../../api'
+import api from '../../api'
 import { BUTTON_TYPES, makeCallbackButton } from '../buttons'
 import { getEventCard } from '../messages'
 
 export async function sendCard(eventID: number, ctx: ContextMessageUpdate) {
     const event = await api.get(eventID)
 
-    const remindButton = makeCallbackButton('Напомнить', BUTTON_TYPES['reminder:event'], { eventID: event.id })
+    const remindButton = makeCallbackButton('Напомнить', BUTTON_TYPES['reminder-ask'], { eventID: event.id })
     const message = getEventCard(event)
 
     ctx.replyWithHTML(message, {
