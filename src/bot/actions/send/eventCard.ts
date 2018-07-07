d29e8f6127d074efa268d6db5f58cca8798fd38a
Andrew Pashinnik <tobirawork@gmail.com>
2018-07-08 01:00:29 +0300

    refactor(bot/actions): move send.ts to bot/actions/send/eventCard.ts

--- src/bot/actions/send/eventCard.ts
+++ src/bot/actions/send/eventCard.ts
@@ -1,18 +1,18 @@
 import { ContextMessageUpdate } from 'telegraf'
-import api from '../../api'
-import { BUTTON_TYPES, makeCallbackButton } from '../buttons'
-import { getEventCard, replies } from '../messages'
+import api from '../../../api'
+import { BUTTON_TYPES, makeCallbackButton } from '../../buttons'
+import { eventsMessages } from '../../messages'
 import { AxiosError } from 'axios'
 
-export async function sendCard(eventID: number, ctx: ContextMessageUpdate) {
+export async function sendEventCard(eventID: number, ctx: ContextMessageUpdate) {
     const event = await api.get(eventID).catch((err: AxiosError) => {
-        if (err.response.status === 404) ctx.replyWithHTML(replies.eventNotFound)
+        if (err.response.status === 404) ctx.replyWithHTML(eventsMessages.static.eventNotFound)
     })
 
     if (!event) return
 
     const remindButton = makeCallbackButton('Напомнить', BUTTON_TYPES['reminder-ask'], { eventID: event.id })
-    const message = getEventCard(event)
+    const message = eventsMessages.getEventCard(event)
 
     ctx.replyWithHTML(message, {
         reply_markup: { inline_keyboard: [[remindButton]] },
